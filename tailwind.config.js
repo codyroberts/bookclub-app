21ba25cc1f5f691975e4d3fe3b462c5b7a187e04
jquinzell <>
2024-05-15 15:48:12 -0500

    extend color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
-    colors: {
-      darkest: "#6b9080",
-      dark: "#a4c3b2",
-      mid: "#cce3de",
-      light: "#eaf4f4",
-      lightest: "#f6fff8",
-      black: "#000000",
-      white: "#FFFFFF",
+    extend: {
+      colors: {
+        darkest: "#6b9080",
+        dark: "#a4c3b2",
+        mid: "#cce3de",
+        light: "#eaf4f4",
+        lightest: "#f6fff8",
+        black: "#000000",
+        white: "#FFFFFF",
+      },
     },
   },
   plugins: [],
